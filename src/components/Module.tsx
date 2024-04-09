4f567b9d66f51fa5cc0f4f33cf676ed5669d08d7
Gustavo Seabra <devguga87@gmail.com>
2024-04-09 15:18:00 -0300

    feat: passa como prop para lesson, qual é a lesson correta

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -15,8 +15,10 @@ interface ModuleProps{
 export function Module({ moduleIndex, title, amountOfLessons}:ModuleProps){
     const dispatch = useDispatch()
 
-    const lessons = useAppSelector(state => {
-        return state.player.course.modules[moduleIndex].lessons
+    const {currentModuleIndex, currentLessonIndex,lessons} = useAppSelector(state => {
+        const {currentLessonIndex,currentModuleIndex} = state.player
+        const lessons = state.player.course.modules[moduleIndex].lessons
+        return {currentModuleIndex, currentLessonIndex, lessons}
     })
 
     return (
@@ -31,12 +33,18 @@ export function Module({ moduleIndex, title, amountOfLessons}:ModuleProps){
             </Collapsible.Trigger>
             <Collapsible.Content>
                 <nav className='relative flex flex-col gap-4 p-6'>
-                    {lessons.map((lesson, lessonIndex) => <Lesson 
-                        key={lesson.id} 
-                        title={lesson.title} 
-                        duration={lesson.duration}
-                        onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-                    />)}
+                    {lessons.map((lesson, lessonIndex) => {
+                        const isCurrent = currentLessonIndex === lessonIndex && moduleIndex === currentModuleIndex
+                        return (
+                            <Lesson 
+                                key={lesson.id} 
+                                title={lesson.title} 
+                                duration={lesson.duration}
+                                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                                isCurrent={isCurrent}
+                            />
+                        )
+                    })}
                 </nav>
             </Collapsible.Content>
         </Collapsible.Root>
